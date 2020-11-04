514f4d6c8b3a95aa05f1d7689ebe9b47895c33ec
Isotta Landi <landii03@Isottas-MacBook-Pro.local>
2020-11-04 15:47:03 -0500

    create long/wide datasets

---
+++ BioMe/R_scripts/create_long.R
@@ -0,0 +1,113 @@
+# Script that organizes longitudinal data within the BioMe BioBank program.
+# Steps:
+# 1. read the .txt tables (i.e., Social_History, Encounter_Diagnosis,
+# Vitals, Medications)
+# 2. include all terms adding a column with a term description
+
+# Required libraries
+require(data.table)
+require(dplyr)
+require(tidyr)
+require(stringr)
+
+# Data path
+data_path <- "/sc/hydra/work/lepowl01/BioMe_Pull"
+# File names
+subj_list_file <- "masterrgnidlist.csv"
+social_history_file<- "Social_History.txt"
+diagn_file <- "Encounter_Diagnosis.txt"
+vitals_file <- "Vitals.txt"
+med_file <- "Medications.txt"
+
+#DELETE ME
+ICD10<-diag_df[dx_code==ICD10,]*** #FIGURE THIS OUT LATER
+
+# Import data
+subj_df <- fread(file.path(data_path, subj_list_file), sep = "|",
+                 stringsAsFactors = FALSE, header = TRUE)
+sh_df <- fread(file.path(data_path, social_history_file),
+               sep = "|", stringsAsFactors = FALSE, header = TRUE)
+diag_df <- fread(file.path(data_path, diagn_file),
+               sep = "|", stringsAsFactors = FALSE, header = TRUE)
+vitals_df <- fread(file.path(data_path, vitals_file),
+               sep = "|", stringsAsFactors = FALSE, header = TRUE)
+med_df <- fread(file.path(data_path, med_file),
+               sep = "|", stringsAsFactors = FALSE, header = TRUE)
+
+
+#############################################
+# Create longitudal dataset: long orientation
+#############################################
+
+# Order tables wrt subject id and encounter date
+setorder(vitals_df, rgnid, encounter_date)
+setorder(diag_df, rgnid, encounter_date)
+setorder(med_df, rgnid, encounter_date)
+setorder(sh_df, rgnid, encounter_date)
+
+# Select columns and row-bind datasets
+
+# VITALS: create new column ("vs_code") pasting vitals and unit of measure
+# Select: vs_code, vital_sign_description
+# Add label column
+vitals_df$vs_code <- paste(vitals_df$vital_sign_measurement, 
+                           vitals_df$vital_sign_unit_of_measure, sep="::")
+vitals_rid <- vitals_df[, .(rgnid, encounter_date, 
+                            code=vs_code,
+                            description=vital_sign_description)]
+vitals_rid$label <- rep("vitals", nrow(vitals_rid))
+
+# DIAGNOSIS
+diag_df$dx_code <- paste(diag_df$dx_code1, 
+                         diag_df$dx_code_type, sep = "::")
+diagn_rid <- diag_df[, .(rgnid, encounter_date,
+                          code = dx_code,
+                          description = dx_name)]
+diagn_rid$label <- rep("diagnosis", nrow(diagn_rid))
+
+# SOCIAL HISTORY
+sh_2 <- data.table::melt(sh_df[,.(rgnid, encounter_date, 
+                           smoking=TOBACCO_USER, 
+                           alcohol=IS_ALCOHOL_USER, 
+                           drugs=IS_ILL_DRUG_USER, 
+                           sex=SEXUALLY_ACTIVE)],
+                           id.vars = c('rgnid','encounter_date'),
+                           variable.name = 'description',
+                           value.name = 'code')
+
+sh_rid<-sh_2[, .(rgnid, encounter_date, code, description)]
+
+sh_rid$label <- rep('sh', nrow(sh_rid))
+
+# MEDICATIONS
+med_rid <- med_df[, .(rgnid, encounter_date, 
+                            code=DESCRIPTION,
+                            description=SIG)]
+med_rid$label <- rep("meds", nrow(med_rid))
+
+# Merge all
+df_long<-rbind(diagn_rid, vitals_rid, sh_rid, med_rid)
+
+setorder(df_long, rgnid, encounter_date)
+nrow(df_long) #30248776
+
+what_na <- df_long[is.na(code),]
+
+#drop 177479 NA's in sh
+df_long_c<- df_long[!is.na(code),]
+
+# Save complete 
+fwrite(df_long_c, sep="|", 
+       file= file.path(data_path,"longitudinal_assessments.txt"))
+# Save EHRs for patients with at least two encounters and that have been 
+# in the EHR system for at least a year
+
+long_df_co <- df_long_c[rgnid %in% subj_df$rgnid,]
+
+setorder(long_df_co, rgnid, encounter_date)
+long_df_cohort<-long_df_co[!duplicated(long_df_co), ]
+
+fwrite(long_df_cohort, sep="|", 
+       file= file.path(data_path,"longitudinal_assessments.txt"))
+
+
---
+++ BioMe/R_scripts/create_wide.R
@@ -0,0 +1,93 @@
+# Script that organizes data collected once within the BioMe BioBank program.
+# Steps:
+# 1. read the .txt tables (i.e., Questionnaire, Demographics)
+# 2. drop columns that have more than 50% information missing
+# 3. selected columns 
+# 4. concatenate the different data by column into a unique dataframe
+
+# Required libraries
+require(data.table)
+require(dplyr)
+require(tidyr)
+require(stringr)
+
+# Data paths
+data_path <- "/sc/hydra/work/lepowl01/BioMe_Output_Files"
+# Files
+subj_list_file <- "masterrgnidlist.csv"
+quest_file <- "Questionnaire.txt"
+demographics_file <- "Demographics.txt"
+
+#Import data
+# List of subjects that have been in EHR for at 
+# least 1 year and has at least 2 visits (or encounters? Is it different?).
+subj_df <- fread(file.path(data_path, subj_list_file), sep="|",
+            stringsAsFactors=FALSE, header=TRUE)
+# Questionnaire
+quest_df <- fread(file.path(data_path, quest_file), sep="|",
+                  stringsAsFactors=FALSE, header=TRUE)
+# Social history
+sochis_df <- fread(social_history_path, sep="|",
+                   stringsAsFactors=FALSE, header=TRUE)
+# Demographics
+demo_df <- fread(demographics_path, sep="|",
+                 stringsAsFactors=FALSE, header=TRUE)
+
+#########################################
+# Wide dataset (variables collected once)
+#########################################
+# Drop duplicated rows
+quest_df <- quest_df[!duplicated(quest_df), ]
+#length(which(duplicated(quest_df))) == 0
+
+#Number of repeated subjects (id column "rgnid")
+count_id<-quest_df[,.N,rgnid][order(-N)]
+table(count_id$N) 
+#    1      2     3
+#  31551   294    11
+
+# Why subjects have N>2 questionnaire entries? 
+# Was a new version of the questionnaire administered? (i.e., after 2012?)
+# We should probably consider the most complete entries instead of baseline data.
+# Select the entry that maximizes the available information.
+repeated_id <- count_id[N>=2]$rgnid
+quest_repeated <- quest_df[rgnid %in% repeated_id] 
+setorder(quest_repeated, rgnid, SPECIMEN_COLLECTION_DATE_TIME)
+repeated_id$na_counts <- rowSums(is.na(quest_repeated))
+repeated_id <- repeated_id[repeated_id[, .I(which.min(count_na)), rgnid]$V1]
+
+# Drop subjects with repeated questionnaires and add the selected ones
+quest_df <- quest_df[!rgnid %in% repeated_id]
+quest_df <- rbind(quest_df, repeated_id[,!"count_na"])
+setorder(quest_df, rgnid, ENROLLMENT_DATE, 
+         SPECIMEN_COLLECTION_DATE_TIME) 
+nrow(quest_u) #1856, yes this= uniqueN(quest$rgnid) 
+
+# Add demographic information (drop duplicates and check if repeated info exist)
+# and drop columns with more than 50% of
+# data missing
+demo_df <- demo_df[!duplicated(demo_df)]
+#length(which(duplicated(demo_df)))
+
+# Check of repeated observation exist (e.g., marital status can change)
+table(demo_dfp[,.N,rgnid][order(-N)])
+
+# if not...
+dt_all = merge(quest_df, demo_df, by="rgnid", all=TRUE) 
+#nrow(dt_all): 32300
+
+#keep only columns with <50% missing data
+id_col <- names(dt_all)[which(colSums(is.na(dt_all))<(nrow(dt_all)/2))]
+dt_all <- dt_all[, ..id_col]
+#Check which columns satisfy the condition
+# names(dt_all)
+
+# Save complete dataset with non-repeated measures
+fwrite(dt_all, sep="|", 
+       file= file.path(data_path,"single_assessments.txt"))
+# Save dataset selecting only subjects with at least 2 visits 
+# and that have been in EHR at least a year.
+dt_rid <- dt_all[rgnid %in% subj_list_file$rgnid]
+nrow(dt_rid) # 27837
+fwrite(dt_rid, sep="|", 
+       file= file.path(data_path, "single_assessments_cohort.txt"))
